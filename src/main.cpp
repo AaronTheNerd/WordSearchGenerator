3cef88e7b9f9b527da28756e97dee65649ef5d2c
AaronTheNerd <bargeaaron@gmail.com>
2021-07-29 20:01:27 -0400

    Made extremely filled word search

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
     if (argc > 1) {
         seed = atoi(argv[1]);
     }
-    std::vector<std::string> word_bank = {"subliminal", "hardworker", "lmao", "determined", "hireme", "cpp", "python", "java", "matlab", "problemsovler", "organized", "bestcandidate", "programmer", "criticalthinker", "resilient", "class", "integer", "double", "objectoriented", "struct", "videogames", "boardgames", "sudoku", "puzzle", "jess", "linux", "verilog"};
+    std::vector<std::string> word_bank = {"subliminal", "hardworker", "lmao", "determined", "hireme", "cpp", "python", "java", "matlab", "problemsovler", "organized", "bestcandidate", "programmer", "criticalthinker", "resilient", "class", "integer", "double", "objectoriented", "struct", "videogames", "boardgames", "sudoku", "puzzle", "jess", "linux", "verilog", "recursive", "code"};
     atn::word_search<15, 15> puzzle(seed, word_bank);
     std::cout << puzzle.to_string() << std::endl;
 }
